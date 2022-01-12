d58ac1b38ce8f5956b918326dc938634ab04169a
krigjo25 <73070534+krigjo25@users.noreply.github.com>
2022-01-12 17:56:33 +0100

    Update patientJournal.py

--- HospitalManagementSystem/python/patientJournal.py
+++ HospitalManagementSystem/python/patientJournal.py
@@ -14,11 +14,12 @@ from reportlab.pdfgen.canvas import Canvas
 load_env()
 
 class CreatePDF(Canvas):
-    def __init__(self, filename='patientJournal.pdf'):
+    def __init__(self, filename='patientJournal.pdf', pagesize=(595.27,841.89), bottomup=1, pageCompression=0, encoding=rl_config.defaultEncoding, verbosity=0, encrypt):
+        super().__init__(filename,pagesize, bottomup, pageCompression, encoding, verbosity, encrypt)
         self.conn = connect(
                             user= getenv('USERNAME'),
                             port= int(getenv('PORT')),
                             password = getenv('PASSWORD'),
                             database = getenv('DATABASE')
         )
-        pass
+https://www.reportlab.com/docs/reportlab-userguide.pdf
