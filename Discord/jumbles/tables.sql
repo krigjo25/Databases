73b7fa0bc136234e6e7f58eaf833067b26b3aff9
krigjo25 <krigjo25@gmail.com>
2023-01-26 07:42:44 +0100

    initial commit

--- Discord/jumbles/tables.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE OR REPLACE TABLE waltdisney ( 
-                            id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                            characters VARCHAR(255) NOT NULL,
-                            roles VARCHAR(255) NOT NULL,
-                            animation VARCHAR(255) NOT NULL,
-                            img MEDIUMBLOB );
